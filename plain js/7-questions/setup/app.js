a5e2ce71756c483dc5fc3ee5d6669ee6a9c7ab80
caitlingbailey <64934329+caitlingbailey@users.noreply.github.com>
2020-06-15 14:03:33 +0100

    Accordion articles JS

--- plain js/7-questions/setup/app.js
+++ plain js/7-questions/setup/app.js
@@ -1,2 +1,28 @@
 //using selectors inside the element
-// traversing the dom
+//looks inside item you have already selected
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question){
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click', function(){
+        questions.forEach(function(item){
+            if(item !== question){
+                item.classList.remove('show-text');
+            };
+        });
+        question.classList.toggle('show-text')
+    });
+});
+
+
+// //traversing the DOM method
+
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(function(btn){
+//     btn.addEventListener('click', function(event){
+//         const question = event.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     });
+// });
+
